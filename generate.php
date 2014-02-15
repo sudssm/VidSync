ef8bae6e5fa907fbfdfbb077213bbd81302fc38f
Mary <maryx@sas.upenn.edu>
2014-02-15 17:35:19 -0500

    generate php

--- generate.php
+++ generate.php
@@ -9,14 +9,17 @@
     //Generate session
     $apiObj = new OpenTokSDK(API_Config::API_KEY, API_Config::API_SECRET);
     $session = $apiObj->create_session();
-//    echo "\nsession: ";
-//    echo $session->getSessionId();
+    echo "\nsession: ";
+    echo $session->getSessionId();
 
     //Generate token
     $sdk = new OpenTokSDK(API_Config::API_KEY,API_Config::API_SECRET);
 
-    string json_encode(string $creds [, string $sessionId = $session->getSessionId() [, string $token = $sdk->generate_token($session->getSessionId())]]);
-   
+
+    $creds = array('sessionId'=>$session->getSessionId(), 'token'=>$sdk->generate_token($session->getSessionId());
     echo $creds;
+    echo json_encode($creds);
+   
+ 
 ?>
 
