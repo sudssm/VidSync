4d62120031b76c2851883d8081c4976104bc92f3
Mary <maryx@sas.upenn.edu>
2014-02-15 16:06:29 -0500

    forgot semicol

--- webcam.php
+++ webcam.php
@@ -2,9 +2,6 @@
 <html>
 <title>test</title>
     <head>
-        <meta http-equiv="X-UA-Compatible" content="chrome=1">
-        <script src="https://swww.tokbox.com/webrtc/v2.0/js/TB.min.js" type="text/javascript" charset="utf-8"></script>
-        <script type="text/javascript" charset="utf-8">
 
 <?php
     require_once 'SDK/API_Config.php';
@@ -22,7 +19,11 @@
     print $sdk->generate_token(sessionId);
     ?>
 
-        var apiKey = 44651662 //<?php print API_Config::API_KEY?>;
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <script src="https://swww.tokbox.com/webrtc/v2.0/js/TB.min.js" type="text/javascript" charset="utf-8"></script>
+        <script type="text/javascript" charset="utf-8">
+
+        var apiKey = 44651662; //<?php print API_Config::API_KEY?>;
         var sessionId = 'fcf66a2cc7ed40498c38b655851f6082717fc48c';//'<?php print $session; ?>';
         var token = 'T1==cGFydG5lcl9pZD00NDY1MTY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz04NzM0N2FkZjQwZWY1MDY3ZDc2ZmMyOWFjN2U2OTRiNjQ0Yzk1OWQxOnJvbGU9c3Vic2NyaWJlciZzZXNzaW9uX2lkPWZjZjY2YTJjYzdlZDQwNDk4YzM4YjY1NTg1MWY2MDgyNzE3ZmM0OGMmY3JlYXRlX3RpbWU9MTM5MjQ5Njg0NSZub25jZT0wLjgzNTMyODgwMTQ3MDM2NzcmZXhwaXJlX3RpbWU9MTM5NTA4ODgyOCZjb25uZWN0aW9uX2RhdGE9';
 //'<?php print $sdk->generate_token($sessionId); ?>';
