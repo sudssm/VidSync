34a0f1ed549f853d1a8052498302b6f879632b5c
Mary <maryx@sas.upenn.edu>
2014-02-15 15:27:59 -0500

    design

--- webcam.php
+++ webcam.php
@@ -1,3 +1,4 @@
+
 <?php include 'SDK/API_Config.php'?>
 <?php include 'SDK/OpenTokArchive.php'?>
 <?php include 'SDK/OpenTokSDK.php'?>
@@ -7,35 +8,48 @@
 <html>
 <head>
 <script src='http://static.opentok.com/webrtc/v2.0/js/TB.min.js' ></script>
+<?php
+    require_once 'API_Config.php';
+    require_once 'OpenTokSDK.php';
+ 
+    $apiObj = new OpenTokSDK(API_Config::API_KEY, API_Config::API_SECRET);
+    $session = $apiObj->create_session();
+    echo $session->getSessionId();
+?>
 </head>
 
 <body>
-<script type='text/javascript'>
-var apiKey = <?php print API_Config::API_KEY?>;
-var sessionId = '<?php print $sessionId; ?>';
-var token = '<?php print $apiObj->generate_token($sessionId); ?>';
-
-console.log(apiKey);
-console.log(sessionId);
-console.log(token);
-
-// -- Create session 
-//  var publisher = TB.initPublisher(apiKey, 'myPublisher', {width:400, height:300});
+<script type="text/javascript">
+  var apiKey = <?php print API_Config::API_KEY?>;
+  var sessionId = '<?php print $sessionId; ?>';
+  var token = '<?php print $apiObj->generate_token($sessionId); ?>';
+ 
+  function sessionConnectedHandler (event) {
+     session.publish( publisher );
+     subscribeToStreams(event.streams);
+  }
+  function subscribeToStreams(streams) {
+    for (var i = 0; i < streams.length; i++) {
+        var stream = streams[i];
+        if (stream.connection.connectionId 
+               != session.connection.connectionId) {
+            session.subscribe(stream);
+        }
+    }
+  }
+  function streamCreatedHandler(event) {
+    subscribeToStreams(event.streams);
+  }
+ 
   var publisher = TB.initPublisher(apiKey);
   var session   = TB.initSession(sessionId);
-
+ 
   session.connect(apiKey, token);
   session.addEventListener("sessionConnected", 
                            sessionConnectedHandler);
  
   session.addEventListener("streamCreated", 
                            streamCreatedHandler);
-
-  session.publish(publisher); 
-
 </script>
-
-hello??
 </body>
-
 </html>
