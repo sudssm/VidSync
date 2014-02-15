f36beb63becdfb42c17b78ec33a6d738e1a25a5a
Mary <maryx@sas.upenn.edu>
2014-02-15 16:17:24 -0500

    change var

--- webcam.php
+++ webcam.php
@@ -7,16 +7,21 @@
     require_once 'SDK/API_Config.php';
     require_once 'SDK/OpenTokSDK.php';
 
+
+    echo "apiKey: ";
+    echo API_Config::API_KEY;
+
+
     //Generate session
     $apiObj = new OpenTokSDK(API_Config::API_KEY, API_Config::API_SECRET);
     $session = $apiObj->create_session();
-    echo "session: ";
+    echo "\nsession: ";
     echo $session->getSessionId();
 
     //Generate token
     $sdk = new OpenTokSDK(API_Config::API_KEY,API_Config::API_SECRET);
     // Replace with the correct session ID:
-    echo "token: ";
+    echo "\ntoken: ";
     echo $sdk->generate_token($session->getSessionId());
     ?>
 
@@ -24,18 +29,18 @@
         <script src="https://swww.tokbox.com/webrtc/v2.0/js/TB.min.js" type="text/javascript" charset="utf-8"></script>
         <script type="text/javascript" charset="utf-8">
 
-        var apiKey = 44651662; //<?php print API_Config::API_KEY?>;
-        console.log(apiKey);
+        var apiKey = <?php print API_Config::API_KEY?>;
         var sessionId = '<?php print $session->getSessionId(); ?>';
-        var token = 'T1==cGFydG5lcl9pZD00NDY1MTY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz04NzM0N2FkZjQwZWY1MDY3ZDc2ZmMyOWFjN2U2OTRiNjQ0Yzk1OWQxOnJvbGU9c3Vic2NyaWJlciZzZXNzaW9uX2lkPWZjZjY2YTJjYzdlZDQwNDk4YzM4YjY1NTg1MWY2MDgyNzE3ZmM0OGMmY3JlYXRlX3RpbWU9MTM5MjQ5Njg0NSZub25jZT0wLjgzNTMyODgwMTQ3MDM2NzcmZXhwaXJlX3RpbWU9MTM5NTA4ODgyOCZjb25uZWN0aW9uX2RhdGE9';
-//'<?php print $sdk->generate_token($sessionId); ?>';
+        var token = '<?php print $sdk->generate_token($session->getSessionId()); ?>';
 
             TB.addEventListener("exception", exceptionHandler);
             var session = TB.initSession('fcf66a2cc7ed40498c38b655851f6082717fc48c'); // Replace with your own session ID. See https://dashboard.tokbox.com/projects
             TB.setLogLevel(TB.DEBUG);
             session.addEventListener("sessionConnected", sessionConnectedHandler);
             session.addEventListener("streamCreated", streamCreatedHandler);
-            session.connect(44651662, 'T1==cGFydG5lcl9pZD00NDY1MTY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz04NzM0N2FkZjQwZWY1MDY3ZDc2ZmMyOWFjN2U2OTRiNjQ0Yzk1OWQxOnJvbGU9c3Vic2NyaWJlciZzZXNzaW9uX2lkPWZjZjY2YTJjYzdlZDQwNDk4YzM4YjY1NTg1MWY2MDgyNzE3ZmM0OGMmY3JlYXRlX3RpbWU9MTM5MjQ5Njg0NSZub25jZT0wLjgzNTMyODgwMTQ3MDM2NzcmZXhwaXJlX3RpbWU9MTM5NTA4ODgyOCZjb25uZWN0aW9uX2RhdGE9'); // Replace with your API key and token. See https://dashboard.tokbox.com/projects
+            session.connect(apiKey, token);
+
+//44651662, 'T1==cGFydG5lcl9pZD00NDY1MTY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz04NzM0N2FkZjQwZWY1MDY3ZDc2ZmMyOWFjN2U2OTRiNjQ0Yzk1OWQxOnJvbGU9c3Vic2NyaWJlciZzZXNzaW9uX2lkPWZjZjY2YTJjYzdlZDQwNDk4YzM4YjY1NTg1MWY2MDgyNzE3ZmM0OGMmY3JlYXRlX3RpbWU9MTM5MjQ5Njg0NSZub25jZT0wLjgzNTMyODgwMTQ3MDM2NzcmZXhwaXJlX3RpbWU9MTM5NTA4ODgyOCZjb25uZWN0aW9uX2RhdGE9'); // Replace with your API key and token. See https://dashboard.tokbox.com/projects
 
             function sessionConnectedHandler(event) {
                  console.log("connected");
