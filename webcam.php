7beba411fea9f98764dcf432a851bf5a56de3539
Mary <maryx@sas.upenn.edu>
2014-02-15 16:11:09 -0500

    blah

--- webcam.php
+++ webcam.php
@@ -10,12 +10,14 @@
     //Generate session
     $apiObj = new OpenTokSDK(API_Config::API_KEY, API_Config::API_SECRET);
     $session = $apiObj->create_session();
-    echo "session" + $session->getSessionId();
+    echo "session";
+    echo $session->getSessionId();
 
     //Generate token
     $sdk = new OpenTokSDK(API_Config::API_KEY,API_Config::API_SECRET);
     // Replace with the correct session ID:
-    echo "token" +  $sdk->generate_token(sessionId);
+    echo "token";
+    echo $sdk->generate_token(sessionId);
     ?>
 
         <meta http-equiv="X-UA-Compatible" content="chrome=1">
@@ -24,16 +26,16 @@
 
         var apiKey = 44651662; //<?php print API_Config::API_KEY?>;
         console.log(apiKey);
-        var sessionId = 'fcf66a2cc7ed40498c38b655851f6082717fc48c';//'<?php print $session; ?>';
+        var sessionId = 'fcf66a2cc7ed40498c38b655851f6082717fc48c'; //'<?php print $session; ?>';
         var token = 'T1==cGFydG5lcl9pZD00NDY1MTY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz04NzM0N2FkZjQwZWY1MDY3ZDc2ZmMyOWFjN2U2OTRiNjQ0Yzk1OWQxOnJvbGU9c3Vic2NyaWJlciZzZXNzaW9uX2lkPWZjZjY2YTJjYzdlZDQwNDk4YzM4YjY1NTg1MWY2MDgyNzE3ZmM0OGMmY3JlYXRlX3RpbWU9MTM5MjQ5Njg0NSZub25jZT0wLjgzNTMyODgwMTQ3MDM2NzcmZXhwaXJlX3RpbWU9MTM5NTA4ODgyOCZjb25uZWN0aW9uX2RhdGE9';
 //'<?php print $sdk->generate_token($sessionId); ?>';
 
             TB.addEventListener("exception", exceptionHandler);
-            var session = TB.initSession(sessionId); // Replace with your own session ID. See https://dashboard.tokbox.com/projects
+            var session = TB.initSession('fcf66a2cc7ed40498c38b655851f6082717fc48c'); // Replace with your own session ID. See https://dashboard.tokbox.com/projects
             TB.setLogLevel(TB.DEBUG);
             session.addEventListener("sessionConnected", sessionConnectedHandler);
             session.addEventListener("streamCreated", streamCreatedHandler);
-            session.connect(apiKey, token); // Replace with your API key and token. See https://dashboard.tokbox.com/projects
+            session.connect(44651662, 'T1==cGFydG5lcl9pZD00NDY1MTY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz04NzM0N2FkZjQwZWY1MDY3ZDc2ZmMyOWFjN2U2OTRiNjQ0Yzk1OWQxOnJvbGU9c3Vic2NyaWJlciZzZXNzaW9uX2lkPWZjZjY2YTJjYzdlZDQwNDk4YzM4YjY1NTg1MWY2MDgyNzE3ZmM0OGMmY3JlYXRlX3RpbWU9MTM5MjQ5Njg0NSZub25jZT0wLjgzNTMyODgwMTQ3MDM2NzcmZXhwaXJlX3RpbWU9MTM5NTA4ODgyOCZjb25uZWN0aW9uX2RhdGE9'); // Replace with your API key and token. See https://dashboard.tokbox.com/projects
 
             function sessionConnectedHandler(event) {
                  console.log("connected");
