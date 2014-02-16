3f27ede9f538150fd23cee1f53b9839208364fbd
Mary <maryx@sas.upenn.edu>
2014-02-16 06:21:44 -0500

    chat css

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function incoming (fb) {
     tokboxToken = data.token;
     showWelcomeMessage();
     $("#chat").show();
-    $("#chat").prepend("<h1 id='roomName'>pizza</h1>");
+    $("#chat").prepend("<h1>"+roomName+"</h1>");
 
     runWebcam(tokboxSession, tokboxToken);
   }
