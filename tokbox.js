00180206f25fd2b92282a8b04b8bbceee1fd482b
Mary <maryx@sas.upenn.edu>
2014-02-16 02:20:29 -0500

    header

--- tokbox.js
+++ tokbox.js
@@ -27,7 +27,7 @@ function runWebcam(sessionId, token){
 		var random = Math.random().toString(36).substring(7);
 		console.log(random);
 //		$("#cams").append('<div id=\''+rand+'\'</div>');
-		session.subscribe(stream, null, {width:200, height:150});
+		document.getElementById('#cams').innerHTML = session.subscribe(stream, null, {width:200, height:150});
 //		session.publish(subscriber);
 //		session.subscribe(stream);
             }
